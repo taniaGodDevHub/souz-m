fb568a03649331d819a23bdfecb4f51c8b7e3ab8
Sky <sky@sky.sky>
2025-07-31 14:21:06 +0300

    Выгрузка в тест

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'crm.souz-m',
     'language' => 'ru-RU',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
