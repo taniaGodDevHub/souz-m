25ca12a1c1dc501b56bbb9c34b77f6694fe163aa
Sky <sky@sky.sky>
2025-08-07 15:42:18 +0300

    Получение заказа и отправка сообщения

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -62,6 +62,8 @@ class OrderController extends AccessController
     {
         $data = json_decode(file_get_contents('php://input'), true);
 
+        return $data;
+
         if(empty($data->client_name)){
             throw new BadRequestHttpException('client_name is invalid');
         }
