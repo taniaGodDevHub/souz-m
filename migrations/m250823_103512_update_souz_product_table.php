cec6fe582e44fb11b6170beaa080d426d5c81a24
Sky <sky@sky.sky>
2025-08-23 13:37:06 +0300

    Получение заказа и отправка сообщения

---
+++ migrations/m250823_103512_update_souz_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+class m250823_103512_update_souz_product_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('souz_product', 'attributes', $this->text(3000));
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m250823_103512_update_souz_product_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250823_103512_update_souz_product_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
