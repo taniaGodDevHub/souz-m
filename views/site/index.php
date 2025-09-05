a9058d58417d3ad8b52c66a5836ba1008ca64ab2
Sky <sky@sky.sky>
2025-09-05 15:09:20 +0300

    Сменили бота

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,12 @@ $this->title = 'crm.eds-systems';
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
         <h1 class="display-4">crm.souz-m</h1>
 
-        <p class="lead">Тут можно придумать какой-то приветственный текст или сделать дашборд со статистикой или общей информацией.</p>
+        <p class="lead">
+        <div class="row justify-content-center">
+            <div class="col-md-4 col-12">Добрый день! При первом входе не забудьте заполнить профиль.</div>
+            <div class="col-md-4 col-12"></div>
+        </div>
+        </p>
 
     </div>
 </div>
