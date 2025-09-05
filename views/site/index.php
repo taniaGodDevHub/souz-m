670cefede8ab23d758115a280c3e813717b69c4c
Sky <sky@sky.sky>
2025-09-05 15:13:58 +0300

    Сменили бота

--- views/site/index.php
+++ views/site/index.php
@@ -12,14 +12,17 @@ $this->title = 'crm.eds-systems';
         <h1 class="display-4">crm.souz-m</h1>
 
         <p class="lead">
-        <div class="col-md-4 col-12 text-center">Добрый день! </div>
+            <div class="row justify-content-center">
 
-            <?php if(!Yii::$app->user->isGuest): ?>
-                <div class="col-md-4 col-12 text-center">При первом входе не забудьте заполнить профиль :)</div>
-                <div class="col-md-4 col-12 text-center">
-                    <a class="btn btn-success" href="<?= Url::to(['/user-profile/update', 'user_id' => Yii::$app->user->identity->id])?>">Перейти в профиль</a>
-                </div>
-            <?php endif;?>
+                <div class="col-md-4 col-12 text-center">Добрый день! </div>
+
+                <?php if(!Yii::$app->user->isGuest): ?>
+                    <div class="col-md-4 col-12 text-center">При первом входе не забудьте заполнить профиль :)</div>
+                    <div class="col-md-4 col-12 text-center">
+                        <a class="btn btn-success" href="<?= Url::to(['/user-profile/update', 'user_id' => Yii::$app->user->identity->id])?>">Перейти в профиль</a>
+                    </div>
+                <?php endif;?>
+            </div>
         </p>
 
     </div>
