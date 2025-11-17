aa1fc7dff77784230656f0132898d899937e62f8
Sky <sky@sky.sky>
2025-11-17 17:47:06 +0300

    Города

--- views/city/_form.php
+++ views/city/_form.php
@@ -14,6 +14,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'name')->dropDownList([
+        0 => 'Нет',
+        1 => 'Да',
+    ]) ?>
+
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
