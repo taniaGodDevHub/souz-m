5f2e6ccf6d3b457505e48e9e7d21978f40f8cc54
Sky <sky@sky.sky>
2026-01-31 11:56:28 +0300

    Восстановление контроллера data и моделей

---
+++ models/OrderProduct.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order_product".
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property string $name
+ */
+class OrderProduct extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order_product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['order_id', 'name'], 'required'],
+            [['order_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'order_id' => 'Order ID',
+            'name' => 'Name',
+        ];
+    }
+
+}
